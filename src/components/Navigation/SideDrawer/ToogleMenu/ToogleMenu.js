fd29bbea8728f28420d4b24328ea48052c918578
mariam-fouad <mariam.fouad94@gmail.com>
2018-08-30 01:14:32 +0300

    remove the menu icon on big screan

--- src/components/Navigation/SideDrawer/ToogleMenu/ToogleMenu.js
+++ src/components/Navigation/SideDrawer/ToogleMenu/ToogleMenu.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import classes from './ToogleMenu.css'
 const toogleMenu =(props)=>(
-  <div onClick={props.toogle}>
-    <div className={classes.MenuIcon} style={{backgroundColor:props.color}}></div>
-    <div className={classes.MenuIcon} style={{backgroundColor:props.color}}></div>
-    <div className={classes.MenuIcon} style={{backgroundColor:props.color}}></div>
+  <div onClick={props.toogle} className={classes.MenuIcon}>
+    <div style={{backgroundColor:props.color}}></div>
+    <div style={{backgroundColor:props.color}}></div>
+    <div style={{backgroundColor:props.color}}></div>
   </div>
 )
 
