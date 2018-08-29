f760362c896513de6c36cd15d57b1ba9f312ce24
mariam-fouad <mariam.fouad94@gmail.com>
2018-08-30 00:25:11 +0300

    added life hocks to the model to render when necessry only

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React,{Component} from 'react';
 import classes from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop';
-const modal =(props)=>{
-  const animateModal ={
-    transform: props.show?  'translateY(0)':'translateY(-100vh)',
-    opacity:props.show?  '1':'0',
-  };
-  return (
-    <React.Fragment>
-      <Backdrop show={props.show} clicked={props.clickModal}/>
-      <div className={classes.Modal} style={animateModal}>
-        {props.children}
-      </div>
-    </React.Fragment>
-  );
+class Modal extends Component{
+
+  //only render the modal when it is necessry
+  shouldComponentUpdate(nextProps,nextState){
+    return this.props.show!=nextProps.show;
+  }
+  render(){
+    const animateModal ={
+      transform: this.props.show?  'translateY(0)':'translateY(-100vh)',
+      opacity:this.props.show?  '1':'0',
+    };
+    return (
+      <React.Fragment>
+        <Backdrop show={this.props.show} clicked={this.props.clickModal}/>
+        <div className={classes.Modal} style={animateModal}>
+          {this.props.children}
+        </div>
+      </React.Fragment>
+    );
+  }
+
 }
 
-export default modal;
+export default Modal;
