1455829fb4d73e55eb5a8d72462e2d99cbd7fe10
mariam-fouad <mariam.fouad94@gmail.com>
2018-09-13 02:27:48 +0300

    add ContactData component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React , {Component} from 'react';
+import {Route} from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 class Checkout extends Component{
   state={
     ingredients:{
@@ -32,7 +34,7 @@ class Checkout extends Component{
           ingredients={this.state.ingredients}
           checkoutCancal={this.checkoutCancalHandler}
           checkoutContinue={this.checkoutContinueHandler}/>
-
+          <Route path={this.props.match.path+"/contact-data"} component={ContactData}/>
       </div>
     )
   }
