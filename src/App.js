7999bf956e98c582604dfcf738cb0546151680b2
mariam-fouad <mariam.fouad94@gmail.com>
2018-09-12 22:48:46 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter , Switch , Route} from 'react-router-dom'
 import './App.css';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -7,14 +8,18 @@ import Checkout from './containers/Checkout/Checkout'
 class App extends Component {
   render() {
     return (
-      <StyleRoot>
-        <div className="App">
-          <Layout>
-            <BurgerBuilder/>
-          </Layout>
-          <Checkout/>
-        </div>
-      </StyleRoot>
+      <BrowserRouter>
+        <StyleRoot>
+          <div className="App">
+            <Layout>
+              <Switch>
+                <Route exact path="/Checkout" component={Checkout}/>
+                <Route exact path="/" component={BurgerBuilder}/>
+              </Switch>
+            </Layout>
+          </div>
+        </StyleRoot>
+      </BrowserRouter>
     );
   }
 }
