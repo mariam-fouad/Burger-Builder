2de6c3c4eed4addd50f67796fe3d6c2e66ceb8b9
mariam-fouad <mariam.fouad94@gmail.com>
2018-09-20 02:37:29 +0300

    add the Route for the Orders page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch , Route} from 'react-router-dom'
 import './App.css';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Orders from './containers/Orders/Orders';
 import Radium ,{StyleRoot}from 'radium';
 import Checkout from './containers/Checkout/Checkout'
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
             <Layout>
               <Switch>
                 <Route  path="/checkout" component={Checkout}/>
+                <Route  path="/orders" component={Orders}/>
                 <Route exact path="/" component={BurgerBuilder}/>
               </Switch>
             </Layout>
