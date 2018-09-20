f7dd5ed8b708b6a9ccf4789359faa42cf3999c37
mariam-fouad <mariam.fouad94@gmail.com>
2018-09-20 22:20:34 +0300

    update the form with the Input component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import axios from '../../../order-axios';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 class ContactData extends Component{
   state={
     name:'',
@@ -49,10 +50,10 @@ class ContactData extends Component{
       <React.Fragment>
         <h4>Enter your Contact Data</h4>
         <form>
-          <input type='text' name='name' placeholder='Your Name'/>
-          <input type='email' name='email' placeholder='Your Email'/>
-          <input type='text' name='street' placeholder='Street'/>
-          <input type='text' name='postalcode' placeholder='postal code'/>
+          <Input inputtype='input' type='text' name='name' placeholder='Your Name'/>
+          <Input inputtype='input' type='email' name='email' placeholder='Your Email'/>
+          <Input inputtype='input' type='text' name='street' placeholder='Street'/>
+          <Input inputtype='input' type='text' name='postalcode' placeholder='postal code'/>
           <Button click={this.orderHandler}>ORDER</Button>
         </form>
       </React.Fragment>
