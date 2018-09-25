6a71b1f58e5b3a0e3a41a8ffb5830129b767120e
mariam-fouad <mariam.fouad94@gmail.com>
2018-09-25 21:37:48 +0300

    setup redux-thunk

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from 'react-router-dom';
-import {createStore} from 'redux';
+import {createStore,applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './store/reducers/rootReducer';
-const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+import thunk from 'redux-thunk';
+const store = createStore(reducer,applyMiddleware(thunk));
 ReactDOM.render((
   <Provider store={store}>
     <BrowserRouter>
