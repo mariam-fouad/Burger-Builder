4f2db3b37133bd7ef35e50306e5ce22560bf1bbe
mariam-fouad <mariam.fouad94@gmail.com>
2018-09-25 18:07:04 +0300

    enable Redux DevTools Extension

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from 'react-router-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './store/reducers/rootReducer';
-const store = createStore(reducer);
+const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 ReactDOM.render((
   <Provider store={store}>
     <BrowserRouter>
