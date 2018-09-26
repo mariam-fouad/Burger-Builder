495b0c341cb4d6c9e7f249bda8c9dbf9861d212e
mariam-fouad <mariam.fouad94@gmail.com>
2018-09-26 03:37:24 +0300

    add the orderReduce to the rootReducer of the redux

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import burgerReducer from './burgerReducer';
+import orderReducer from './orderReducer';
 const reducer= (state={},action)=>{
   return {
     burgerReducer:burgerReducer(state.burgerReducer,action),
+    orderReducer: orderReducer(state.orderReducer,action),
   };
 }
 export default reducer;
