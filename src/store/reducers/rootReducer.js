596f7c91e1614dd535965cf455e8e5c96901ad99
mariam-fouad <mariam.fouad94@gmail.com>
2018-09-28 00:36:00 +0300

    add the authReducer to the rootReducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,9 +1,11 @@
 import burgerReducer from './burgerReducer';
 import orderReducer from './orderReducer';
+import authReducer from './authReducer';
 const reducer= (state={},action)=>{
   return {
     burgerReducer:burgerReducer(state.burgerReducer,action),
     orderReducer: orderReducer(state.orderReducer,action),
+    authReducer: authReducer(state.authReducer,action),
   };
 }
 export default reducer;
