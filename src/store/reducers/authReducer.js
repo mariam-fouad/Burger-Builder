4ffbb2213c8fbed14623116195c96c09534b503f
mariam-fouad <mariam.fouad94@gmail.com>
2018-09-28 00:34:33 +0300

    implment the Auth reducer logic

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -3,7 +3,7 @@ import {
   AUTH_SUCCESS,
   AUTH_FAILED,
 } from '../actions/actionTypes';
-
+import {updateObject} from '../utility'
 const initialState = {
   token:null,
   userId:null,
@@ -11,9 +11,29 @@ const initialState = {
   loading:false,
 };
 
-const reducer = (state,action){
+const authLoading = (state,action)=>{
+  return updateObject (state,{loading:true,error:null});
+};
+
+const authSuccess =(state,action)=>{
+  return updateObject(state,{
+    loading:false,
+    error:null,
+    token:actoin.token,
+    userId:action.userId,});
+};
+
+const authFailed = (state,action)=>{
+  return updateObject (state,{
+    loading:false,
+    error:action.errorMsg,
+  });
+}
+const reducer = (state=initialState,action){
   switch (action.type) {
-    
+    case AUTH_LOADING : return authLoading (state,action);
+    case AUTH_SUCCESS : return authSuccess(state,action);
+    case AUTH_FAILED  : return authFailed (state,action);
     default:return state;
   }
 }
