e0333786119ba7eeec699637a3131d47d24576f6
mariam-fouad <mariam.fouad94@gmail.com>
2018-09-27 20:15:49 +0300

    create authActions file

---
+++ src/store/actions/authActions.js
@@ -0,0 +1,39 @@
+import {
+  AUTH_LOADING,
+  AUTH_SUCCESS,
+  AUTH_FAILED,
+} from './actionTypes';
+import axios from 'axios';
+
+const authLoading = ()=>{
+  return {
+    type:AUTH_LOADING,
+  };
+}
+
+const authSuccess= ()=>{
+  return {
+    type:AUTH_SUCCESS
+  }
+}
+
+const authFailed = (error)=>{
+  return {
+    type:AUTH_FAILED,
+    error:error,
+  }
+}
+export const authStart= (email,password)=>{
+  return dispatch=>{
+    dispatch(authLoading());
+    axios.post ('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=AIzaSyAHKdQmU1l-OX0c-xPEC3Bjh8p_gFM3V44')
+      .then(response=>{
+        console.log(response);
+        dispatch(authSuccess());
+      })
+      .catch(error=>{
+        console.log(error);
+        dispatch(authFailed(error));
+      })
+  }
+}
