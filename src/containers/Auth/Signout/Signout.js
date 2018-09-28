bdcd3c08fe9514fb9e75ba46edbd7a804fac90e9
mariam-fouad <mariam.fouad94@gmail.com>
2018-09-28 03:27:05 +0300

    redirect when visiting the signout link

--- src/containers/Auth/Signout/Signout.js
+++ src/containers/Auth/Signout/Signout.js
@@ -1,12 +1,13 @@
 import React ,{Component} from 'react';
 import {connect} from 'react-redux';
+import {Redirect} from 'react-router-dom';
 import * as actions from '../../../store/actions/actionsIndex';
 class Signout extends Component{
   componentDidMount(){
     this.props.sigingout();
   }
   render(){
-    return <h1>ghhg</h1>;
+    return <Redirect to='/'/>;
   }
 }
 
