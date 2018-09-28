053cd671aa6c998e4dd47cd31d91138d57fca27c
mariam-fouad <mariam.fouad94@gmail.com>
2018-09-28 03:04:05 +0300

    pass the isAuth to the NavigationItems

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -11,7 +11,7 @@ const toolbar =(props)=>{
         <Logo/>
       </div>
       <nav className={classes.DisplayDesktop}>
-        <NavigationItems/>
+        <NavigationItems isAuth={props.isAuth}/>
       </nav>
     </header>
   );
