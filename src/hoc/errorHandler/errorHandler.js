9cf12e3494b39542a57d8386ea729ab31a809902
mariam-fouad <mariam.fouad94@gmail.com>
2018-09-28 03:01:11 +0300

    fix the bug of clicking in the backdrop to close

--- src/hoc/errorHandler/errorHandler.js
+++ src/hoc/errorHandler/errorHandler.js
@@ -40,7 +40,7 @@ const errorHandler= ( WrappedComponent, axios ) => {
                 <React.Fragment>
                     <Modal
                         show={this.state.error}
-                        modalClosed={this.errorConfirmedHandler}>
+                        clickModal={this.errorConfirmedHandler}>
                         {this.state.error ? this.state.error.message : null}
                     </Modal>
                     <WrappedComponent {...this.props} />
