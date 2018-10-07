3e6e6c1951fefa29e5d6794281645aea6a9c6705
mariam-fouad <mariam.fouad94@gmail.com>
2018-10-07 23:38:48 +0300

    test number of NavigationItem rendered if user is not Auth

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {configure,shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+configure({adapter: new Adapter()});
+describe('<NavigationItems/>',()=>{
+  it('should return two <NavigationItem/> if not Auth',()=>{
+    const wrapper = shallow(<NavigationItems/>);
+    expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+});
