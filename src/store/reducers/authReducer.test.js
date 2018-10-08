b424e18f9ad8a91b3f1338df8d22fc2c9b272282
mariam-fouad <mariam.fouad94@gmail.com>
2018-10-08 14:35:24 +0300

    make sure that authReducer update the state correctly

--- src/store/reducers/authReducer.test.js
+++ src/store/reducers/authReducer.test.js
@@ -10,8 +10,22 @@ import {
 import {initialState} from './authReducer.js'
 
 describe ('authReducer',()=>{
+
   it ('should initial the state with default state',()=>{
     expect(reducer(undefined,{})).toEqual(initialState);
+  });
+
+  it ('should update the state with token and userId',()=>{
+    expect(reducer(initialState,{
+      type:AUTH_SUCCESS,
+      token:'token',
+      userId:'userId',
+    })).toEqual({
+      ...initialState,
+      token:'token',
+      userId:'userId',
+    });
+    
   });
 
 });
