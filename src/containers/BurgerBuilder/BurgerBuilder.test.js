6b4258e682772629c465d714e294698a7bc3a002
mariam-fouad <mariam.fouad94@gmail.com>
2018-10-08 13:32:37 +0300

    test BuildControls not rendered if the ingredients props is missing

--- src/containers/BurgerBuilder/BurgerBuilder.test.js
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -18,4 +18,8 @@ describe('<BurgerBuilder/>',()=>{
     expect(wrapper.find(BuildControls)).toHaveLength(1);
   });
 
+  it("shouldn't render BuildControls if ingredients props doesn't exist",()=>{
+    expect(wrapper.find(BuildControls)).toHaveLength(0);
+  });
+
 });
