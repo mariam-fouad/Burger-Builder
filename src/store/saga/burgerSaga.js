f253d03fe9dab6767d90080382588c7ea49a8c07
Mariam Fouad <mariam.fouad94@gmail.com>
2018-10-30 16:55:29 +0300

    fix unwated import and action missing in the burgerSaga

--- src/store/saga/burgerSaga.js
+++ src/store/saga/burgerSaga.js
@@ -1,9 +1,8 @@
 import * as burgerActions from '../actions/burgerActions';
-import {delay} from 'redux-saga';
 import {put} from 'redux-saga/effects';
 import axios from '../../order-axios';
 
-export function* initialIngredient (){
+export function* initialIngredient (action){
     try {
         const response = yield axios.get('/ingredients.json');
         yield put (burgerActions.setIngredient(response.data));
