ee44d64153d5ec82c64f0c75dd59c7232f15e01c
Mariam Fouad <mariam.fouad94@gmail.com>
2018-10-30 14:11:09 +0300

    implment the authSignout in the authSaga

---
+++ src/store/saga/authSaga.js
@@ -0,0 +1,19 @@
+import {
+    AUTH_LOADING,
+    AUTH_SUCCESS,
+    AUTH_FAILED,
+    AUTH_SIGNOUT,
+    SET_AUTH_REDIRECT_PATH,
+  } from './actionTypes';
+
+  import {put} from 'redux-saga/effects';
+
+function* authSignout(action){
+    yield localStorage.removeItem('expiryData');
+    yield localStorage.removeItem('token');
+    yield localStorage.removeItem('userId');
+
+    yield put({
+          type:AUTH_SIGNOUT,
+        });
+}
