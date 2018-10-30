f06ee10391a7932df8efc67d7b68c7c7d71c4126
Mariam Fouad <mariam.fouad94@gmail.com>
2018-10-30 14:46:12 +0300

    connect the sagamiddleware to the store

--- src/index.js
+++ src/index.js
@@ -8,9 +8,15 @@ import {createStore,applyMiddleware,compose} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './store/reducers/rootReducer';
 import thunk from 'redux-thunk';
+import createSagaMiddleWare from 'redux-saga';
+import {watchAuth} from './store/saga';
 
 const composeEnhancers =process.env.NODE_ENV==='development'? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
-const store = createStore(reducer,composeEnhancers(applyMiddleware(thunk)));
+
+const sagaMiddleware = createSagaMiddleWare();
+const store = createStore(reducer,composeEnhancers(applyMiddleware(thunk,sagaMiddleware)));
+sagaMiddleware.run(watchAuth);
+
 ReactDOM.render((
   <Provider store={store}>
     <BrowserRouter>
